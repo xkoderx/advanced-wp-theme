7f81f4956389d5ca1c9ecc4ea93c12757c02a23c
Reyacido <xerickxmichelex@gmail.com>
2024-09-03 11:41:45 -0600

    html

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+
+<head>
+    <title>Advanced WP Theme</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>Advanced Wordpress Theme</h1>
+            <div class="h_right">
+                <form>
+                    <input type="text" placeholder="Search...">
+                </form>
+            </div>
+        </div>
+    </header>
+    <nav class="nav main-nav">
+        <div class="container">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="#">Services</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container content">
+        <div class="main block">
+            <article class="post">
+                <h1>Blog Post 1</h1>
+                <p class="meta">Posted at 11:00 on May 9 by admin</p>
+                aliqua. Incididunt ut ullamco ut ut. Excepteur eiusmod cillum nostrud consequat adipisicing dolor amet
+                laboris aliquip in labore. Ipsum officia ut ea cillum.
+            </article>
+            <article class="post">
+                <h1>Blog Post 1</h1>
+                <p class="meta">Posted at 11:00 on May 9 by admin</p>
+                aliqua. Incididunt ut ullamco ut ut. Excepteur eiusmod cillum nostrud consequat adipisicing dolor amet
+                laboris aliquip in labore. Ipsum officia ut ea cillum.
+            </article>
+            <article class="post">
+                <h1>Blog Post 1</h1>
+                <p class="meta">Posted at 11:00 on May 9 by admin</p>
+                aliqua. Incididunt ut ullamco ut ut. Excepteur eiusmod cillum nostrud consequat adipisicing dolor amet
+                laboris aliquip in labore. Ipsum officia ut ea cillum.
+            </article>
+            <a class="button" href="#">Read More</a>
+        </div>
+    </div>
+    <footer class="main-footer">
+        <div class="container">
+            <div class="f_left">
+                <p>&amp;copy; 2024 - Advanced WP Theme</p>
+            </div>
+            <div class="f_right">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
